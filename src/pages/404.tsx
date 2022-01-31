6a05e615934673eb7c08b615f95ff5d612252af5
Franek Marciniak <prostyfranek@protonmail.com>
2022-01-31 15:38:04 +0100

    small change

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,7 +6,7 @@ const NotFoundPage: React.FC = () => (
   <Layout>
     <SEO title="404: Not found" />
     <div style={{ height: "50vh", textAlign: "center", marginTop: "150px" }}>
-      <h1>Page not found</h1>
+      <h1>Page under construction</h1>
     </div>
   </Layout>
 );
